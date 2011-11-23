96c2bd74bab579c1d33cb22f00f7d7ad22286f58
Johan Andersson <johan474@zaza12.ida.liu.se>
2011-11-23 15:20:15 +0100

    Johan testar

---
+++ territory.h
@@ -0,0 +1,23 @@
+//territory.h
+#include <string>
+#include <vector>
+
+class Territory
+{
+ public:
+  Territory(std::string, std::vector<std::string>);
+  bool owned_by(std::string);
+  void set_owner(); 
+  void add_armies();
+  void remove_armies();
+ 
+ std::string get_owner();
+  int get_armies();
+
+  private:
+  std::string name;
+  int armies;
+  std::string owner;
+  std::vector<std::string> adjacent;
+
+};
